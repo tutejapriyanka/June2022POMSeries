7d542c8ac198bf5892377884c5dcdf7507614948
tutejapriyanka <priyanka.tuteja84@gmail.com>
2022-10-26 14:12:10 +0530

    updated the code after peer review

--- src/main/java/com/qa/opencart/pages/AddCartPage.java
+++ src/main/java/com/qa/opencart/pages/AddCartPage.java
@@ -1,8 +1,16 @@
 package com.qa.opencart.pages;
 
+import org.openqa.selenium.By;
+
 public class AddCartPage {
 
 	
 	String name ="iMac";
 	String price ="200$";
+	
+	By loc =By.id("loc");
+	
+	public void click() {
+		System.out.println("Click --"+ loc);
+	}
 }
