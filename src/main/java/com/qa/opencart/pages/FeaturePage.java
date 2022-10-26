301d765b2b37d8ebb58ef3d9fa8053c4d2255433
tutejapriyanka <priyanka.tuteja84@gmail.com>
2022-10-26 20:37:22 +0530

    f2 feature

--- src/main/java/com/qa/opencart/pages/FeaturePage.java
+++ src/main/java/com/qa/opencart/pages/FeaturePage.java
@@ -2,4 +2,12 @@ package com.qa.opencart.pages;
 
 public class FeaturePage {
 
+	
+	public void f1() {
+		System.out.println("f1 feature");
+	}
+	
+	public void f2() {
+		System.out.println("f2 feature");
+	}
 }
